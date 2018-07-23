e348f3330ec63d5c2ffbfe922e6ed956562f5592
Pat Cavit <github@patcavit.com>
2018-07-22 23:54:47 -0700

    Setting up a bit

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    extends : [
+        "@tivac",
+        "plugin:jest/recommended",
+    ],
+    parserOptions : {
+        ecmaVersion : 8,
+    },
+    env : {
+        node : true,
+        jest : true,
+        es6  : true,
+        browser : true,
+    },
+
+    plugins : [
+        "jest",
+    ]
+};
